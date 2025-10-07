24e8a81b268262145b152994c0ef59eca714d769
Drivasx <davidmrivasb2004@gmail.com>
2025-10-07 12:38:33 -0500

    secure our api w arcjet

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import logger from '#config/logger.js';
 import { formatValidationError } from '#utils/format.js';
+import { createUser } from '../services/auth.service.js';
 import { signUpSchema } from '../validations/auth.validation.js';
+import jwttoken from '#utils/jwt.js';
+import {cookies} from '#utils/cookies.js';
 
 export const signup = async (req, res, next) => {
   try {
@@ -11,7 +14,13 @@ export const signup = async (req, res, next) => {
       return res.status(400).json({ error: formattedError });
     }
 
-    const { name, email, role } = validationResult.data;
+    const { name, email, role, password } = validationResult.data;
+
+    const user = await createUser({name, email, password, role });
+
+    const token = jwttoken.sign({ id: user.id, email: user.email, role: user.role });
+
+    cookies.set(res, 'token', token);
 
     logger.info(`User registered successfully: ${email}`);
 
@@ -19,7 +28,7 @@ export const signup = async (req, res, next) => {
       .status(201)
       .json({
         message: 'User registered successfully',
-        user: { id: 1, name, email, role },
+        user: { id: user.id, name: user.name, email: user.email, role: user.role },
       });
   } catch (error) {
     logger.error('Error in signup', error);
